e213c3759b4bc11c048ecdc34c822b7e08346512
Benjamin Harris <Bigben83@users.noreply.github.com>
2025-02-05 20:05:26 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,63 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'nokogiri'
+require 'open-uri'
+require 'sqlite3'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find something on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+# Initialize the SQLite database
+db = SQLite3::Database.new "development_applications.db"
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Create the table if it doesn't exist
+db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS breakoday (
+    id INTEGER PRIMARY KEY,
+    description TEXT,
+    date_received TEXT,
+    address TEXT,
+    council_reference TEXT,
+    applicant TEXT,
+    owner TEXT,
+    stage_description TEXT,
+    stage_status TEXT,
+    document_description TEXT
+  );
+SQL
+
+# Define the URL of the page
+url = 'https://www.bodc.tas.gov.au/council/advertised-development-applications/'
+
+# Fetch and parse the HTML content
+html = URI.open(url)
+doc = Nokogiri::HTML(html)
+
+# Define variables for storing extracted data for each entry
+address = ''  
+description = ''
+date_received = ''
+council_reference = ''
+applicant = ''
+owner = ''
+stage_description = ''
+stage_status = ''
+document_description = ''
+
+
+# Find the table containing the development applications
+table = doc.at_css('table') # Adjust the selector as needed
+
+# Iterate through each row in the table
+table.css('tr').each do |row|
+  # Extract the columns
+  columns = row.css('td')
+  next if columns.empty? # Skip rows without data
+
+  # Extract the text content of each column
+  name = columns[0].text.strip
+  address = columns[1].text.strip
+  closing_date = columns[2].text.strip
+  pdf_link = columns[3].css('a').first['href'] rescue nil
+
+  # Insert the data into the database
+  db.execute("INSERT INTO applications (name, address, closing_date, document_description) VALUES (?, ?, ?, ?)",
+             [name, address, closing_date, pdf_link])
+end
+
+puts "Data has been successfully inserted into the database."
